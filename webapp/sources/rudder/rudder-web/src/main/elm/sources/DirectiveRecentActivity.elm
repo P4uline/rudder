3b8c69defdedd5700579da97e317b012fc3eb8da
pauline.iogna <pauline.iogna@gmail.com>
2026-06-10 17:41:52 +0200

    clean - add subscriptions method

--- webapp/sources/rudder/rudder-web/src/main/elm/sources/DirectiveRecentActivity.elm
+++ webapp/sources/rudder/rudder-web/src/main/elm/sources/DirectiveRecentActivity.elm
@@ -1,12 +1,11 @@
 module DirectiveRecentActivity exposing (..)
 
-import Activity.ApiCalls exposing (copy, errorNotification)
-import Activity.DataTypes exposing (Activity, Msg(..))
+import Activity.ApiCalls exposing (copy, getUrl, processApiError)
+import Activity.DataTypes exposing (Activity, ContextPath)
 import Activity.InitTooltips exposing (initTooltips)
-import Activity.JsonDecoder exposing (decodeErrorDetails, decodeGetActivities)
+import Activity.JsonDecoder exposing (decodeGetActivities)
 import Activity.JsonEncoder exposing (encodeRestEventLogFilter)
 import Browser
-import Dict
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import Http exposing (header, jsonBody, request)
@@ -16,28 +15,22 @@ import Ordering exposing (Ordering)
 import Rudder.Table exposing (..)
 import Task
 import Time exposing (Posix, Zone, millisToPosix, utc)
-import TimeZone
-import Url.Builder exposing (QueryParameter)
 import Http exposing (Error)
 import Http.Detailed
 
 
 
 
-type alias DirectiveId =
-    String
-
-
+type alias DirectiveId = String
 
 type alias Model =
     { directiveId : DirectiveId
     , activityTable : Rudder.Table.Model Activity Msg
-    , contextPath : String
+    , contextPath : ContextPath
     , activities : List Activity
     , currentTime : Posix
     , zone : Zone
     }
-
 
 type Msg
     = RudderTableMsg (Rudder.Table.Msg Msg)
@@ -45,13 +38,6 @@ type Msg
     | GetActivities (Result (Http.Detailed.Error String) ( Http.Metadata, (List Activity) ))
     | Tick Posix
     | Copy String
-
-
-
-getUrl: Model -> List String -> List QueryParameter -> String
-getUrl m url p=
-  Url.Builder.relative (m.contextPath :: "secure" :: "api" :: url) p
-
 
 initTable : List Activity -> Rudder.Table.Model Activity Msg
 initTable activities =
@@ -74,9 +60,7 @@ initTable activities =
                             )
                     )
 
-
-
-        data =
+        {-data =
             [ { id = 1
               , actor = "Admin"
               , description = "Awesome directive 1"
@@ -107,38 +91,19 @@ initTable activities =
 
               {- , date=Time.now -}
               }
-            ]
+            ]-}
     in
     -- Rudder.Table.init config data
     Rudder.Table.init config activities
 
-
-{-init : { contextPath : String, timeZone: String } -> ( Model, Cmd Msg )
-init flags =
-    let
-        initTimeZone =
-            Dict.get flags.timeZone TimeZone.zones
-                |> Maybe.withDefault (\() -> Time.utc)
-
-        initModel =
-            Model flags.contextPath [] (Time.millisToPosix 0) (initTimeZone ())
-
-        initActions =
-            [ getActivities initModel
-            , initTooltips ""
-            , Task.perform Tick Time.now
-            ]
-    in
-    ( initModel, Cmd.batch initActions )-}
-
-getActivities : Model -> Cmd Msg
-getActivities model =
+getActivities : ContextPath ->  Cmd Msg
+getActivities contextPath =
   let
     req =
       request
         { method  = "POST"
         , headers = [header "X-Requested-With" "XMLHttpRequest"]
-        , url     = getUrl model [ "eventlog" ] []
+        , url     = getUrl contextPath [ "eventlog" ] []
         , body    = encodeRestEventLogFilter |> jsonBody
         , expect  = Detailed.expectJson GetActivities decodeGetActivities
         , timeout = Nothing
@@ -146,6 +111,7 @@ getActivities model =
         }
   in
     req
+
 
 {- FIXME pass a timezone to the elm app, just as directiveId and contextPath and then use initTimeZone value instead of currentTime -}
 init : { directiveId : String, contextPath : String{-, timeZone: String-} } -> ( Model, Cmd Msg )
@@ -160,7 +126,7 @@ init flags =
         initModel =
             Model flags.directiveId (initTable []) flags.contextPath [] currentTime utc
         initActions =
-            [ getActivities initModel
+            [ getActivities initModel.contextPath
             , initTooltips ""
             , Task.perform Tick Time.now {- FIXME why this ? do it -}
             ]
@@ -169,38 +135,7 @@ init flags =
     ( initModel, Cmd.batch initActions )
 
 
-
-
-processApiError : String -> Detailed.Error String -> Model -> ( Model, Cmd Msg )
-processApiError apiName err model =
-    let
-        message =
-            case err of
-                Detailed.BadUrl url ->
-                    "The URL " ++ url ++ " was invalid"
-
-                Detailed.Timeout ->
-                    "Unable to reach the server, try again"
-
-                Detailed.NetworkError ->
-                    "Unable to reach the server, check your network connection"
-
-                Detailed.BadStatus metadata body ->
-                    let
-                        ( title, errors ) =
-                            decodeErrorDetails body
-                    in
-                    title ++ "\n" ++ errors
-
-                Detailed.BadBody metadata body msg ->
-                    msg
-    in
-    ( model, errorNotification ("Error when " ++ apiName ++ ", details: \n" ++ message) )
-
-
 {- Table of the recent activity -}
-
-
 table: Model -> Html Msg
 table model =
     div [ class "main-table" ] [ Html.map RudderTableMsg (Rudder.Table.view model.activityTable) ]
@@ -228,16 +163,17 @@ update msg model =
                             )
 
                         Err err ->
-                            processApiError "Getting activities list" err model
+                            (model, processApiError "Getting activities list" err)
         Tick newTime ->
              ( { model | currentTime = newTime }, Cmd.none )
         Copy s -> {- FIXME: Why is it useful ? -}
             ( model, copy s )
 
 
-subscriptions model =
-    Sub.none
-
+subscriptions _ =
+    Sub.batch
+            [ Time.every 1000 Tick -- Update of the current time every second
+            ]
 
 main =
     Browser.element
